261fb049f4ee4be3196ce083f876a44ab9fd0727
AlexGundrum <alex_gundrum@yahoo.com>
2025-09-23 22:00:39 -0500

    added some cool hover effects

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+const aura = document.getElementById('cursor-aura');
+
+document.addEventListener('mousemove', (e) => {
+    aura.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
+});
